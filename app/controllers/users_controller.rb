9628fb61c0c965e9aadd92ff38f8f8134b93e8fb
Alexander Shmarkatyuk <matroskind@gmail.com>
2016-02-10 17:17:08 -0500

    test_user info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,8 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+    @user = User.find params[:id]
+    #@trips = Trip.find params[:user_id]
   end
 
   # GET /users/new

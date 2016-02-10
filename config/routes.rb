bdb6175831703753260726f7419b1317772b2ddb
Alexander Shmarkatyuk <matroskind@gmail.com>
2016-02-10 16:21:34 -0500

    nothing

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   resources :comments
   resources :locations
   resources :trips
-  resources :users
+  resources :users do
+    resources :trips
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

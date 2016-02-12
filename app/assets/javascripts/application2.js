d96651130cf10434de08d6ad85262c45f9ba4a9c
Andrew Holz <Holz.Andrew@gmail.com>
2016-02-12 09:34:35 -0500

    push day 2 work

--- app/assets/javascripts/application2.js
+++ app/assets/javascripts/application2.js
@@ -16,7 +16,22 @@
 //= require_tree .
 
 "use strict";
+// var send_data = function(a,b,c,d){
+//   $.ajax({
+//     type: "POST",
+//     dataType: "json",
+//     url: "/trips",
+//     data: {trip:{
+//       address: a,
+//       place_id: b,
+//       lng: c,
+//       lat: d
+//     }},
 
+//     success: function(post){
+//     }
+//   })
+// };
 
 
 
@@ -29,7 +44,7 @@ $(document).ready(function(){
       url: "https://maps.googleapis.com/maps/api/geocode/json",
       data: {
         address: $('.pure-input-rounded').val(),
-        key: ''
+        key: 'AIzaSyCcIN85clPAZA5_m21QDUD27goF_wJo3g4'
       } ,     
       success: function(data){
         console.log("is this working?")
@@ -39,48 +54,26 @@ $(document).ready(function(){
         //debugger;
         var lat = results.geometry.location.lat;
         var lng = results.geometry.location.lng;
+        //send_data(address, place_id, lng, lat);
+        
         moveMap(lat,lng);
-        return address, place_id, lng, lat;
       }
     })
-
 }
-
-
-// var sendData = function(){
-
-//   $.ajax({
-//     type: "POST",
-//     dataType: "json",
-//     url: "/trips/create",
-//     // data: {trip:{
-//     //   address: a,
-//     //   place_id: b,
-//     //   lng: c,
-//     //   lat: d
-//     // }},
-
-//     success: function(post){
-//       debugger;
-//     }
-//   })
-// };
 
 var map;
 function moveMap(lat, lng) {
 
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: lat, lng: lng},
-    zoom: 13,
+    zoom: 10
   });
 }
 
 
     // $(".pure-form").on('submit', getResults );
-    $(document).on('submit', ".pure-form", getResults );
-     $(document).on('submit', ".create_trip", sendData );
+    $(".pure-form").on('submit', getResults );
 
 }); // ready
-
 
 

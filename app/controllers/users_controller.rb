57d4d37850690c13a57028812ce5cd9efc4ac9f0
Ruth Acosta <ruthacosta100@gmail.com>
2016-02-12 09:37:01 -0500

    trying to clear merge

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,24 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-
   # GET /users
   # GET /users.json
   def index
     @user = User.new
   end
-​
   # GET /users/1
   # GET /users/1.json
   def show
     #@trips = Trip.find params[:user_id]
     @user = User.find(params[:id])
   end
-
   # GET /users/1/edit
   def edit
   end
-​
   # POST /users
   # POST /users.json
   def create
     user = User.new(user_params)
     # binding.pry
-
     respond_to do |format|
       if user.save
         format.html { render :index, notice: 'User was successfully created.' }
@@ -35,7 +30,6 @@ class UsersController < ApplicationController
       end
     end
   end
-
   # POST
   def login
     user = User.find_by_email(params[:email])
@@ -52,7 +46,6 @@ class UsersController < ApplicationController
       redirect_to '/'
     end
   end
-
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
@@ -66,20 +59,17 @@ class UsersController < ApplicationController
       end
     end
   end
-​
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
     session[:user_id] = nil
     redirect_to '/login'
   end
-​
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
     end
-​
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:name, :email, :password_digest, :image_url)

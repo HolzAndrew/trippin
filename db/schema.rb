19fafb57b469b88a16e360b7c0d200b222dbb7c2
Alexander Shmarkatyuk <matroskind@gmail.com>
2016-02-12 10:14:21 -0500

    trying to merge and make stuff work

--- db/schema.rb
+++ db/schema.rb
@@ -77,7 +77,7 @@ ActiveRecord::Schema.define(version: 20160211000250) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "password_digest"
+    t.string   "password"
     t.string   "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

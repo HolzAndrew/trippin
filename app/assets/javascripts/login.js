3a355e442d925438fbc33e5f963da5dd1f2cbb93
Yu Yang <ulyssesyangyu@gmail.com>
2016-02-14 15:13:39 -0500

    remove user ajex

--- app/assets/javascripts/login.js
+++ app/assets/javascripts/login.js
@@ -12,48 +12,49 @@ $(document).ready(function(){
 	  $(this).tab('show')
 	})
 
-	$(document).on('click', 'button.signup', function(e){
-		event.preventDefault();
-		// debugger
-		$.ajax({
-			url:"/signup",
-			type: "POST",
-			data: {
-				user: {  name: $('#user_name').val(),  
-								email: $('#user_email').val(),
-								image_url: $('#user_image_url').val(),
-								password: $('#user_password').val()
-							}
-			},
-			error: function () {alert('double check input!')},
-			success: function (data) { 
-				$('div.user_signup').hide();
-				var result = $('<div />').append(data).find('div.user_status').html();
-	      $('div.user_status').html(result);
-	      document.location.replace("/trips");
-			}
-		})
-	})//
+	// $(document).on('click', 'button.signup', function(e){
+	// 	event.preventDefault();
+	// 	// debugger
+	// 	$.ajax({
+	// 		url:"/signup",
+	// 		type: "POST",
+	// 		data: {
+	// 			user: {  name: $('#user_name').val(),  
+	// 							email: $('#user_email').val(),
+	// 							image_url: $('#user_image_url').val(),
+	// 							password: $('#user_password').val()
+	// 						}
+	// 		},
+	// 		error: function () {alert('double check input!')},
+	// 		success: function (data) { 
+	// 			debugger
+	// 			$('div.user_signup').hide();
+	// 			var result = $('<div />').append(data).find('div.user_status').html();
+	//       $('div.user_status').html(result);
+	//       document.location.replace("/trips");
+	// 		}
+	// 	})
+	// })//
 
-	$(document).on('click', 'button.login', function(e){
-		event.preventDefault();
-		// debugger
-		$.ajax({
-			url:"/login",
-			type: "POST",
-			data: {
-				email: $('#email').val(),
-				password: $('#password').val()
-			},
-			error: function () { alert('double check input!') },
-			success: function (data) { 
-				// debugger
-				$('div.user_signup').hide();
-				var result = $('<div />').append(data).find('div.user_status').html();
-	      $('div.user_status').html(result);
-	      document.location.replace("/trips");
-			}
-		})
-	})//
+	// $(document).on('click', 'button.login', function(e){
+	// 	event.preventDefault();
+	// 	// debugger
+	// 	$.ajax({
+	// 		url:"/login",
+	// 		type: "POST",
+	// 		data: {
+	// 			email: $('#email').val(),
+	// 			password: $('#password').val()
+	// 		},
+	// 		error: function () { alert('double check input!') },
+	// 		success: function (data) { 
+	// 			// debugger
+	// 			$('div.user_signup').hide();
+	// 			var result = $('<div />').append(data).find('div.user_status').html();
+	//       $('div.user_status').html(result);
+	//       document.location.replace("/trips");
+	// 		}
+	// 	})
+	// })//
 
 })//ready

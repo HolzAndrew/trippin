aa0304e73e21bea50902f8772bfe7e2578e04cd7
Ruth Acosta <ruthacosta100@gmail.com>
2016-02-14 12:27:07 -0500

    no changes just trying to pull

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,11 +19,11 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    domain: "http://localhost:3000",
+    domain: "gmail.com",
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password:  ENV["GMAIL_PASSWORD"]
+    user_name: ENV["TRIP_GMAIL_USERNAME"],
+    password:  ENV["TRIP_GMAIL_PASSWORD"]
   }
 
   # Print deprecation notices to the Rails logger.
